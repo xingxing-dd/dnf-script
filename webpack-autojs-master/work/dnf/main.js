8df2c41d7a5ecaaa2d289c8954b3daf44955479e
Administrator <xingxingdd@outlook.sg>
2024-06-21 03:14:23 +0800

    xx

--- webpack-autojs-master/work/dnf/main.js
+++ webpack-autojs-master/work/dnf/main.js
@@ -4,12 +4,12 @@ var server = require("./common/socket")
 var storage = storages.create("info.xingxingdd.dnf");
 ui.statusBarColor("#ffffff");
 utils.loadView("loading")
-utils.loadView("home")
 threads.start(function(){
     //utils.downloadServer()
     utils.startServer()
     server.connect()
 });
+utils.loadView("home")
 // var home = require("./view/home")
 // var update = require("./view/update")
 // ui.setContentView(home.view(ui.container))
