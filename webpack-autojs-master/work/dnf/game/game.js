a94ef827983d60fd8c7702b867ac3fbd73970a44
mingxing.ai <mingxing.ai@ipaylinks.com>
2024-07-04 22:55:50 +0800

    tijiao

---
+++ webpack-autojs-master/work/dnf/game/game.js
@@ -0,0 +1,21 @@
+//打开游戏
+exports.open = () => {
+    desc("地下城与勇士：起源").findOne().click()
+    while (true) {
+        //检测是否已经启动
+    }
+}
+
+//进入地图
+exports.enter = () => {
+    while (true) {
+
+    }
+}
+
+//开始玩游戏
+exports.start = () => {
+    while(true) {
+
+    }
+}
